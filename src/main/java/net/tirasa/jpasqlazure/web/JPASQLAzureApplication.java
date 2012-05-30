0ed7c701f862003af9246e731894c9b9874a5136
Francesco Chicchiricco <ilgrosso@apache.org>
2012-05-30 10:11:50 +0200

    Removing basic AUTH from web app

--- src/main/java/net/tirasa/jpasqlazure/web/JPASQLAzureApplication.java
+++ src/main/java/net/tirasa/jpasqlazure/web/JPASQLAzureApplication.java
@@ -19,14 +19,11 @@
 package net.tirasa.jpasqlazure.web;
 
 import net.tirasa.jpasqlazure.web.pages.HomePage;
-import net.tirasa.jpasqlazure.web.pages.Login;
 import org.apache.wicket.Page;
-import org.apache.wicket.authroles.authentication.AbstractAuthenticatedWebSession;
-import org.apache.wicket.authroles.authentication.AuthenticatedWebApplication;
-import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 
-public class JPASQLAzureApplication extends AuthenticatedWebApplication {
+public class JPASQLAzureApplication extends WebApplication {
 
     @Override
     protected void init() {
@@ -40,15 +37,5 @@ public class JPASQLAzureApplication extends AuthenticatedWebApplication {
     @Override
     public Class<? extends Page> getHomePage() {
         return HomePage.class;
-    }
-
-    @Override
-    protected Class<? extends AbstractAuthenticatedWebSession> getWebSessionClass() {
-        return JPASQLAzureSession.class;
-    }
-
-    @Override
-    protected Class<? extends WebPage> getSignInPageClass() {
-        return Login.class;
     }
 }
